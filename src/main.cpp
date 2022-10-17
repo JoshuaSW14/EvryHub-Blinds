1daf0a06e231559dfbceff5ed9d71aee26cc298d
Joshua Symons-Webb <000812836@mohawkcollege.ca>
2022-10-17 18:03:30 -0400

    updated code

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,41 @@
 #include "Wifi.h"
 #include <Stepper.h>
 
+//AWS Pub/Sub Topics & Wifi Config
 #define AWS_IOT_PUBLISH_TOPIC   "evryhub/smartblinds"
 #define AWS_IOT_SUBSCRIBE_TOPIC "evryhub/smartblinds"
+WiFiClientSecure net = WiFiClientSecure();
+PubSubClient client(net);
 
+//LED Pins
 #define PIN_RED    23 // GIOP23
 #define PIN_GREEN  22 // GIOP22
 #define PIN_BLUE   21 // GIOP21
 
+//Stepper Configuration
 const int stepsPerRevolution = 200;
 Stepper myStepper(stepsPerRevolution, 32, 33, 34, 35);
 
+//LDR Sensor Configuration
 int ldrValue;
 const int ldrPin = 36; // or A0
 const int ldrResolution = 12; // Could be 9-12
 
-WiFiClientSecure net = WiFiClientSecure();
-PubSubClient client(net);
+
+void openBlinds(){
+  myStepper.step(stepsPerRevolution);
+  delay(500);
+}
+
+void closeBlinds(){
+  myStepper.step(-stepsPerRevolution);
+  delay(500);
+}
+
+void configBlinds(){
+  delay(500);
+}
+
 
 void messageHandler(char* topic, byte* payload, unsigned int length)
 {
@@ -34,12 +53,12 @@ void messageHandler(char* topic, byte* payload, unsigned int length)
   const char* device = doc["device"];
   const char* action = doc["action"];
 
-  if(String(device) == "led" && String(action) == "on"){
-    Serial.println("Turn on");
-    //digitalWrite(LED, HIGH);  
-  }else if(String(device) == "led" && String(action) == "off"){
-    Serial.println("Turn off");
-    //digitalWrite(LED, LOW);
+  if(String(device) == "blinds" && String(action) == "open"){
+    Serial.println("Open Blinds");
+    openBlinds();
+  }else if(String(device) == "blinds" && String(action) == "close"){
+    Serial.println("Close Blinds");
+    closeBlinds();
   }
 }
 
@@ -55,6 +74,8 @@ void connectAWS()
     delay(500);
     Serial.print(".");
   }
+  
+  //LED Turns Green When Connected to WiFi
   analogWrite(PIN_RED,   0);
   analogWrite(PIN_GREEN, 255);
   analogWrite(PIN_BLUE,  0);
@@ -69,7 +90,6 @@ void connectAWS()
  
   // Create a message handler
   client.setCallback(messageHandler);
- 
   Serial.println("Connecting to AWS IOT");
  
   while (!client.connect(THINGNAME))
@@ -86,18 +106,19 @@ void connectAWS()
  
   // Subscribe to a topic
   client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
- 
   Serial.println("AWS IoT Connected!");
+
+  //LED Turns Blue When Connected to AWS
   analogWrite(PIN_RED,   0);
   analogWrite(PIN_GREEN, 0);
   analogWrite(PIN_BLUE,  255);
 }
- 
+
 void publishMessage()
 {
   StaticJsonDocument<200> doc;
   doc["device"] = "blinds";
-  doc["action"] = "temperature:";
+  doc["action"] = "light:"+String(ldrValue);
   char jsonBuffer[512];
   serializeJson(doc, jsonBuffer); // print to client
 
@@ -112,6 +133,7 @@ void setup() {
   pinMode(PIN_GREEN, OUTPUT);
   pinMode(PIN_BLUE,  OUTPUT);
 
+  //LED Turns RED When Device is Turned On
   analogWrite(PIN_RED,   255);
   analogWrite(PIN_GREEN, 0);
   analogWrite(PIN_BLUE,  0);
@@ -120,23 +142,11 @@ void setup() {
 }
 
 void loop() {
-
-  
-
   //analogReadResolution(ldrResolution);
   //ldrValue = analogRead(ldrPin);
   ldrValue = digitalRead(ldrPin);
-  Serial.println(ldrValue);
 
-	// Serial.println("clockwise");
-	// myStepper.step(stepsPerRevolution);
-	// delay(500);
-
-	// Serial.println("counterclockwise");
-	// myStepper.step(-stepsPerRevolution);
-	// delay(500);
-
-  // publishMessage();
-  // client.loop();
+  publishMessage();
+  client.loop();
   delay(2000);
 }
